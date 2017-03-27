cb21a598bc4ca3649941dcb83f7e5d3bd10ce901
kuba <kubaskibiak@gmail.com>
2017-03-27 15:57:56 +0200

    a

---
+++ sample_db.py
@@ -0,0 +1,28 @@
+
+import cv2
+cam = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('resources/haarcascade_frontalface_alt.xml')
+
+Id='1'
+sampleNum=0
+while(True):
+    ret, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.5, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        
+        #incrementing sample number 
+        sampleNum=sampleNum+1
+        #saving the captured face in the dataset folder
+        cv2.imwrite("dataSet/User."+Id +'.'+ str(sampleNum) + ".jpg", cv2.resize(gray[y:y+h,x:x+w], (200, 200)))
+
+        cv2.imshow('frame',img)
+    #wait for 100 miliseconds 
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+    # break if the sample number is morethan 20
+    elif sampleNum>20:
+        break
+cam.release()
+cv2.destroyAllWindows()
